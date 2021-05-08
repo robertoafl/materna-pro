6b40456f168050acb93623e14ea522aef1e114d5
Roberto A. F. Lisbôa <robertoafl@hotmail.com>
2021-05-07 21:41:24 -0300

    Criando estrutura contato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    //redirectTo: 'folder/Inbox',
+    redirectTo: 'contacts',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  }
+  },
+  {
+    path: 'contact-form',
+    loadChildren: () => import('./contacts/contact-form/contact-form.module').then( m => m.ContactFormPageModule)
+  },
+  {
+    path: 'contact-list',
+    loadChildren: () => import('./contacts/contact-list/contact-list.module').then( m => m.ContactListPageModule)
+  },
+  { path: 'contacts', loadChildren: './contacts/contact-list/contact-list.module#ContactListPageModule' },
+  { path: 'contacts/new', loadChildren: './contacts/contact-form/contact-form.module#ContactFormPageModule' },
+  { path: 'contacts/edit/:id', loadChildren: './contacts/contact-form/contact-form.module#ContactFormPageModule' }
 ];
 
 @NgModule({
