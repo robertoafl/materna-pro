256674a4d5eaac5ada712400cb41e3691b212692
Roberto A. F. Lisbôa <robertoafl@hotmail.com>
2021-05-07 21:55:07 -0300

    Cadastro de contato

--- src/app/contacts/contact-form/contact-form.page.ts
+++ src/app/contacts/contact-form/contact-form.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { Contact } from '../shared/contact';
+import { ContactService } from '../shared/contact.service';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormPage implements OnInit {
 
-  constructor() { }
+  title: string = 'Novo contato';
+  contact: Contact;
+
+  constructor(
+    private contactService: ContactService,
+    private route: ActivatedRoute, //Pegar a rota ativa do angula
+    private toastCtrl: ToastController) { }
 
   ngOnInit() {
+    this.contact = new Contact();
+
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      this.title = 'Editar contato';
+      this.loadContact(parseInt(idParam));
+    }
+  }
+
+  async loadContact(id: number) {
+    this.contact = await this.contactService.getById(id);
+  }
+
+  async onSubmit() {
+    try {
+      const result = await this.contactService.save(this.contact);
+      this.contact.id = result.insertId;
+
+      const toast = await this.toastCtrl.create({
+        header: 'Sucesso',
+        message: 'Contato salvo com sucesso.',
+        color: 'success',
+        position: 'bottom',
+        duration: 3000
+      });
+
+      toast.present();
+    } catch (error) {
+      const toast = await this.toastCtrl.create({
+        header: 'Erro',
+        message: 'Ocorreu um erro ao tentar salvar o Contato.',
+        color: 'danger',
+        position: 'bottom',
+        duration: 3000
+      });
+
+      toast.present();
+    }
   }
 
 }
--- src/app/contacts/contact-list/contact-list.page.ts
+++ src/app/contacts/contact-list/contact-list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import { Contact } from '../shared/contact';
+import { ContactService } from '../shared/contact.service';
 
 @Component({
   selector: 'app-contact-list',
@@ -7,9 +10,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListPage implements OnInit {
 
-  constructor() { }
+  contacts: Contact[] = [];
 
-  ngOnInit() {
+  constructor(
+    private contactService: ContactService,
+    private toastCtrl: ToastController,
+    private alertCtrl: AlertController) { }
+
+  ngOnInit() { } //Roda apenas uma vez, qnd o componente aparece na tela mas qnd voltar do cadastro ele nao seria chamado
+
+  ionViewWillEnter() { //É carregado sempre que entra na tela
+    this.loadContacts();
   }
 
+  async loadContacts() {
+    this.contacts = await this.contactService.getAll();
+  }
+
+  doSerchClear() {
+    this.loadContacts();
+  }
+
+  async doSerchBarChange($event: any) {
+    const value = $event.target.value;
+    if (value && value.length >= 2) {
+      this.contacts = await this.contactService.filter(value);
+    }
+  }
+
+  async delete(contact: Contact) {
+    const alert = await this.alertCtrl.create({
+      header: 'Deletar?',
+      message: `Deseja excluir o contato: ${contact.name}?`,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Excluir',
+          handler: () => {
+            this.executeDelete(contact);
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  async executeDelete(contact: Contact) {
+    try {
+      // Removendo do banco de dados
+      await this.contactService.delete(contact.id);
+
+      // Removendo do array
+      const index = this.contacts.indexOf(contact);
+      this.contacts.splice(index, 1);
+
+      const toast = await this.toastCtrl.create({
+        header: 'Sucesso',
+        message: 'Contato excluído com sucesso.',
+        color: 'success',
+        position: 'bottom',
+        duration: 3000
+      });
+
+      toast.present();
+    } catch (error) {
+      const toast = await this.toastCtrl.create({
+        header: 'Erro',
+        message: 'Ocorreu um erro ao tentar excluir o Contato.',
+        color: 'danger',
+        position: 'bottom',
+        duration: 3000
+      });
+
+      toast.present();
+    }
+  }
 }
