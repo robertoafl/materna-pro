11b207788ec869aac5076f064b22d02e9851b2fc
Roberto A. F. Lisbôa <robertoafl@hotmail.com>
2021-05-09 10:21:59 -0300

    Add menu, rotas, logo e home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    //redirectTo: 'folder/Inbox',
-    redirectTo: 'contacts',
+    redirectTo: 'folder/Inbox',
     pathMatch: 'full'
   },
+
+  { path: 'contacts', loadChildren: './contacts/contact-list/contact-list.module#ContactListPageModule' },
+  { path: 'contacts/new', loadChildren: './contacts/contact-form/contact-form.module#ContactFormPageModule' },
+  { path: 'contacts/edit/:id', loadChildren: './contacts/contact-form/contact-form.module#ContactFormPageModule' },
+
+  { path: 'consultas', loadChildren: './consultas/consulta-list/consulta-list.module#ConsultaListPageModule' },
+  { path: 'consultas/new', loadChildren: './consultas/consulta-form/consulta-form.module#ConsultaFormPageModule' },
+  { path: 'consultas/edit/:id', loadChildren: './consultas/consulta-form/consulta-form.module#ConsultaFormPageModule' },
+
+  { path: 'tarefas', loadChildren: './tarefas/tarefa-list/tarefa-list.module#TarefaListPageModule' },
+  { path: 'tarefas/new', loadChildren: './tarefas/tarefa-form/tarefa-form.module#TarefaFormPageModule' },
+  { path: 'tarefas/edit/:id', loadChildren: './tarefas/tarefa-form/tarefa-form.module#TarefaFormPageModule' },
+  
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
@@ -20,9 +32,6 @@ const routes: Routes = [
     path: 'contact-list',
     loadChildren: () => import('./contacts/contact-list/contact-list.module').then( m => m.ContactListPageModule)
   },
-  { path: 'contacts', loadChildren: './contacts/contact-list/contact-list.module#ContactListPageModule' },
-  { path: 'contacts/new', loadChildren: './contacts/contact-form/contact-form.module#ContactFormPageModule' },
-  { path: 'contacts/edit/:id', loadChildren: './contacts/contact-form/contact-form.module#ContactFormPageModule' },
   {
     path: 'consulta-form',
     loadChildren: () => import('./consultas/consulta-form/consulta-form.module').then( m => m.ConsultaFormPageModule)
